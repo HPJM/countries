e5f213f05d1fbb7b34ec722e523539b2c02abdef
Hugo <hpjm37@gmail.com>
2022-01-29 14:30:49 +0000

    Inject useful functionality into module using Countries

--- lib/countries.ex
+++ lib/countries.ex
@@ -1,18 +1,29 @@
 defmodule Countries do
-  @moduledoc """
-  Documentation for `Countries`.
-  """
+  @path Path.join([__DIR__, "data", "countries.json"])
+  @external_resource @path
+  @all_countries @path |> File.read!() |> Jason.decode!()
+  @codes @all_countries |> Enum.map(&String.to_atom(&1["Code"])) |> Enum.sort()
 
-  @doc """
-  Hello world.
+  defmacro __using__(_) do
+    ast =
+      for %{"Code" => code, "Name" => name} <- @all_countries do
+        atom_code = String.to_atom(code)
 
-  ## Examples
+        quote do
+          def from_code(unquote(code)), do: unquote(name)
+          def from_code(unquote(atom_code)), do: unquote(name)
 
-      iex> Countries.hello()
-      :world
+          def valid_code?(unquote(code)), do: true
+          def valid_code?(unquote(atom_code)), do: true
+        end
+      end
 
-  """
-  def hello do
-    :world
+    quote do
+      unquote_splicing(ast)
+
+      def codes, do: unquote(@codes)
+
+      def valid_code?(_), do: false
+    end
   end
 end
--- test/countries_test.exs
+++ test/countries_test.exs
@@ -2,7 +2,29 @@ defmodule CountriesTest do
   use ExUnit.Case
   doctest Countries
 
-  test "greets the world" do
-    assert Countries.hello() == :world
+  defmodule Example do
+    use Countries
+  end
+
+  describe "use injects functionality" do
+    test "from_code/1 accepts valid string code" do
+      assert Example.from_code("GG") == "Guernsey"
+    end
+
+    test "from_code/1 accepts valid atom code" do
+      assert Example.from_code(:GG) == "Guernsey"
+    end
+
+    test "valid_code?/1 returns whether code exists" do
+      assert Example.valid_code?(:ES)
+      assert Example.valid_code?("VN")
+      refute Example.valid_code?(:GGG)
+    end
+
+    test "codes/0 returns all codes" do
+      for code <- Example.codes() do
+        assert Example.valid_code?(code)
+      end
+    end
   end
 end
