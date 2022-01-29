a5c528849c358646c77a7183b6d92b27b40648c3
Hugo <hpjm37@gmail.com>
2022-01-29 13:49:32 +0000

    Init commit

---
+++ lib/countries.ex
@@ -0,0 +1,18 @@
+defmodule Countries do
+  @moduledoc """
+  Documentation for `Countries`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Countries.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
